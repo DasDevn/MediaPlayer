120ab95aa90b08b2e138dd8359789313c54e9414
DasDevn <w0196059@nscc.ca>
2024-10-10 13:32:30 -0300

    Add project files.

---
+++ DevanEisnor-Assignment1/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DevanEisnor_Assignment1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TagLib.File currentFile;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            //Example of instantiating an OpenFileDialog
+            OpenFileDialog fileDlg = new OpenFileDialog();
+
+            //Create a file filter
+            fileDlg.Filter = "MP3 files(*.mp3)|*.mp3|All files(*.*)|*.*";
+
+            //ShowDialog shows onscreen for the user
+            //By default it return true if the user selects a file and hits "Open"
+            if (fileDlg.ShowDialog() == true)
+            {
+                //The filename property stores the full path that was selected
+                fileNameBox.Text = fileDlg.FileName;
+
+                //Example of creating a TagLib file object, for accessing mp3 metadata
+                currentFile = TagLib.File.Create(fileDlg.FileName);
+
+                //Set the source of the media player element.
+                myMediaPlayer.Source = new Uri(fileDlg.FileName);
+                myMediaPlayer.Play();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Examples of reading tag data from currently selected file.
+            var year = currentFile.Tag.Year;
+            var title = currentFile.Tag.Title;
+            var album = currentFile.Tag.Album;
+
+            tagNameBox.Text = title + " : " + year + album.ToString();
+        }
+    }
+}
