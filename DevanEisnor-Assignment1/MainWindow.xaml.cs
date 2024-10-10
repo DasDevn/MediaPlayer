45eaa428f4c93b26f5d4895191c23cb57d38f280
Devan Eisnor <W0196059@nscc.ca>
2024-10-10 17:29:56 -0300

    Tag working

--- DevanEisnor-Assignment1/MainWindow.xaml.cs
+++ DevanEisnor-Assignment1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Win32;
+using System.IO;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -46,18 +47,75 @@ namespace DevanEisnor_Assignment1
                 //Set the source of the media player element.
                 myMediaPlayer.Source = new Uri(fileDlg.FileName);
                 myMediaPlayer.Play();
+
+                LoadAlbumArt();
             }
         }
-        //Open Tags
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            //Examples of reading tag data from currently selected file.
-            var year = currentFile.Tag.Year;
-            var title = currentFile.Tag.Title;
-            var album = currentFile.Tag.Album;
 
-            tagNameBox.Text = title + " : " + year + album.ToString();
+        //https://learn.microsoft.com/en-us/dotnet/desktop/wpf/graphics-multimedia/how-to-use-a-bitmapimage?view=netframeworkdesktop-4.8
+        //With some help from chatGBT
+        private void LoadAlbumArt()
+        {
+            if (currentFile.Tag.Pictures.Length > 0)
+            {
+                // Get the album art as a byte array
+                var bin = currentFile.Tag.Pictures[0].Data.Data;
+
+                // Convert the byte array to a BitmapImage
+                using (var stream = new MemoryStream(bin))
+                {
+                    var image = new BitmapImage();
+                    image.BeginInit();
+                    image.StreamSource = stream;
+                    image.CacheOption = BitmapCacheOption.OnLoad;
+                    image.EndInit();
+
+                    // Set the source of the Image control to the album art
+                    Mp3Image.Source = image;
+                }
+            }
+            // If no album art is found, do nothing to keep the default image
         }
+        //Open Tags
+        private void Tag_Click(object sender, RoutedEventArgs e)
+        {
+            // Check if an MP3 file is loaded
+            if (currentFile == null)
+            {
+                MessageBox.Show("Please open an MP3 file first.", "No File Loaded", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            // Toggle visibility: if currently visible, collapse it
+            if (tagNameBox.Visibility == Visibility.Visible)
+            {
+                tagNameBox.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                // If currently collapsed, show the tag information
+                var title = currentFile.Tag.Title;
+                var artist = currentFile.Tag.FirstPerformer;
+                var album = currentFile.Tag.Album;
+                var year = currentFile.Tag.Year;
+
+                //clear so it doesnt keep writing
+                tagNameBox.Inlines.Clear();
+
+                //https://stackoverflow.com/questions/24130980/how-can-i-insert-a-newline-into-a-textblock-without-xaml
+                tagNameBox.Inlines.Add(new Run(title) { FontSize = 24, FontWeight = FontWeights.Bold });
+                tagNameBox.Inlines.Add(new LineBreak());
+                tagNameBox.Inlines.Add(new Run(artist) { FontSize = 20, FontStyle = FontStyles.Italic });
+                tagNameBox.Inlines.Add(new LineBreak());
+
+                // Add album and year with smaller font
+                tagNameBox.Inlines.Add(new Run($"{album} ({year})") { FontSize = 16 });
+
+                // Make the TextBlock visible
+                tagNameBox.Visibility = Visibility.Visible;
+            }
+        }
+
         //Play MP3
         private void Play_Click(object sender, RoutedEventArgs e)
         {
